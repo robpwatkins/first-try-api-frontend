ba3fd06694da844bd58d7702031e78a44424cf78
robpwatkins <robpwatkins@gmail.com>
2021-03-30 22:36:43 -0500

    and thats seriously all there is to that

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [item, setItem] = useState('');
+
+  const fetchItem = async () => {
+    const response = await fetch('https://us-central1-first-try-7d9af.cloudfunctions.net/app/api/read/2');
+    const obj = await response.json();
+    setItem(obj.item);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{item}</h1>
+      <button onClick={fetchItem}>Click ME!</button>
     </div>
   );
 }
