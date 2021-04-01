07a4601433432d658f6e094059895f8214575599
robpwatkins <robpwatkins@gmail.com>
2021-03-31 21:34:38 -0500

    ready to test

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { useState } from 'react';
 function App() {
   const [item, setItem] = useState('');
 
-  const fetchItem = async () => {
-    const response = await fetch('https://us-central1-first-try-7d9af.cloudfunctions.net/app/api/read/2');
-    const obj = await response.json();
-    setItem(obj.item);
-  }
+  // const fetchItem = async () => {
+  //   const response = await fetch('https://us-central1-first-try-7d9af.cloudfunctions.net/app/api/read/2');
+  //   const obj = await response.json();
+  //   setItem(obj.item);
+  // }
 
   return (
     <div className="App">
-      <h1>{item}</h1>
-      <button onClick={fetchItem}>Click ME!</button>
+      <h1>heyoo</h1>
+      {/* <button onClick={fetchItem}>Click ME!</button> */}
     </div>
   );
 }
